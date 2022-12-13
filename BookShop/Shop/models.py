85233f238486c801f297149c02f7ffcbaa8f169c
M.A.T <mat.chenari.computer@gmail.com>
2022-12-13 09:24:33 +0330

    add templates folder

--- BookShop/Shop/models.py
+++ BookShop/Shop/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    Image = models.ImageField()
+    Image = models.ImageField(upload_to='media')
     name = models.CharField(max_length=64)
     author = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
     cost = models.IntegerField()
-    
