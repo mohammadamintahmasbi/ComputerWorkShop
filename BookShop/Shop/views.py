fc5812e8c111213e8118fce668930c6ec085e42a
M.A.T <mat.chenari.computer@gmail.com>
2022-12-13 09:35:21 +0330

    add home page

--- BookShop/Shop/views.py
+++ BookShop/Shop/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def Show_Product(request):
+    all = Product.objects.all()
+    return render(request, 'Home.html', {'products':all})
