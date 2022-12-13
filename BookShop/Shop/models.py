ad2fe9c169530516cc80b2bab75eef2318a1effb
M.A.T <mat.chenari.computer@gmail.com>
2022-12-13 07:13:23 +0330

    set urls in app

--- BookShop/Shop/models.py
+++ BookShop/Shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    Image = models.ImageField()
+    name = models.CharField(max_length=64)
+    author = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    cost = models.IntegerField()
+    
